df0362ff54bd8f5a41db03e18e096cdc92970a05
Delamir <71318139+Delamir@users.noreply.github.com>
2020-10-27 15:08:14 +0100

    Changed line 5 to "kost" Added line 6

--- src/bank/Main.java
+++ src/bank/Main.java
@@ -2,7 +2,8 @@ package bank;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hej");
+        System.out.println("Kost");
+        System.out.println("med dig");
     }
 }
 
